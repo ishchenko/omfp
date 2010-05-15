d24bbdf57944b23d13db1881abd0620ca9b41202
Max Ishchenko <max.ishchenko@gmail.com>
2010-05-15 10:04:58 +0400

    Removed unmarshalling without validation

--- omfp-core/src/main/java/net/ishchenko/omfp/OmfpConverter.java
+++ omfp-core/src/main/java/net/ishchenko/omfp/OmfpConverter.java
@@ -22,19 +22,9 @@ public class OmfpConverter {
         Unmarshaller unmarshaller = JAXBContext.newInstance(FictionBook.class).createUnmarshaller();
         unmarshaller.setSchema(schema);
 
-        FictionBook book;
-        try {
-            book = (FictionBook) unmarshaller.unmarshal(input);
-        } catch (JAXBException e) {
-            e.printStackTrace();
-            //todo: log
-            System.out.println("Processing with no validation");
-            unmarshaller.setSchema(schema);
-            book = (FictionBook) unmarshaller.unmarshal(input);
-        }
+        FictionBook book = (FictionBook) unmarshaller.unmarshal(input);
 
         ProcessingContext context = new ProcessingContext();
-
         FictionBookVisitor[] allVisitors = new FictionBookVisitor[visitors.length + 1];
         allVisitors[0] = new LinkCollectingVisitor(context);
         System.arraycopy(visitors, 0, allVisitors, 1, allVisitors.length - 1);
